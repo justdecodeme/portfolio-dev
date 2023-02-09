0146f86c9a0e2d78d2d3fed66fafadb6760319a6
Rakesh Kumar <justdecodeme@hotmail.com>
2023-02-09 21:10:41 +0530

    Completed filters js logic

--- script.js
+++ script.js
@@ -3,6 +3,35 @@
 /* *********** */
 const yearEl = document.getElementById("year");
 const projectsEl = document.getElementById("projects");
+const filterEls = document.querySelectorAll("#filters li");
+
+// filterEls[0].addEventListener('click', function() { 
+// 	console.log('clicked')
+// })
+// filterEls[1].addEventListener('click', function() { 
+// 	console.log('clicked')
+// })
+// filterEls[2].addEventListener('click', function() { 
+// 	console.log('clicked')
+// })
+// filterEls[3].addEventListener('click', function() { 
+// 	console.log('clicked')
+// })
+// filterEls[4].addEventListener('click', function() { 
+// 	console.log('clicked')
+// })
+// filterEls[5].addEventListener('click', function() { 
+// 	console.log('clicked')
+// })
+
+for (let i = 0; i < filterEls.length; i++) {
+	filterEls[i].addEventListener('click', function (e) { 
+		document.querySelector("#filters li.active").classList.remove('active');
+		filterEls[i].classList.add('active');
+		let tag = e.target.id;
+		renderProjects(tag);
+	})
+}
 
 yearEl.innerHTML = new Date().getFullYear();
 
@@ -15,52 +44,64 @@ const projects = [
 		img: "4.png",
 		codeUrl: "https://github.com/justdecodeme/js-practice-01-change-bg",
 		liveUrl: "https://justdecodeme.github.io/js-practice-01-change-bg/",
+		tags: ['html', 'css']
 	},
 	{
 		name: "Slider App",
 		img: "5.png",
 		codeUrl: "https://github.com/justdecodeme/periodic-table",
 		liveUrl: "https://justdecodeme.github.io/periodic-table/day-01-20-periodic-table.html",
+		tags: ['css']
 	},
 	{
 		name: "Calculator App",
 		img: "4.png",
 		codeUrl: "https://github.com/justdecodeme/js-practice-01-change-bg",
 		liveUrl: "https://justdecodeme.github.io/js-practice-01-change-bg/",
+		tags: ['html', 'js']
 	},
 	{
 		name: "Slider App",
 		img: "5.png",
 		codeUrl: "https://github.com/justdecodeme/periodic-table",
 		liveUrl: "https://justdecodeme.github.io/periodic-table/day-01-20-periodic-table.html",
+		tags: ['js']
 	},
 	{
 		name: "Calculator App",
 		img: "4.png",
 		codeUrl: "https://github.com/justdecodeme/js-practice-01-change-bg",
 		liveUrl: "https://justdecodeme.github.io/js-practice-01-change-bg/",
+		tags: ['illustrator', 'photoshop']
 	},
 	{
 		name: "Slider App",
 		img: "5.png",
 		codeUrl: "https://github.com/justdecodeme/periodic-table",
 		liveUrl: "https://justdecodeme.github.io/periodic-table/day-01-20-periodic-table.html",
+		tags: ['photoshop']
 	},
 ];
 
-let output = "";
+function renderProjects(tag) {
+	let output = "";
 
-for (let i = 0; i < projects.length; i++) {
-	output += `<div class="project">
-  <img src="img/project/${projects[i].img}" alt="Calculator Project Image">
-  <div class="content flex">
-    <p>${projects[i].name}</p>
-    <p class="flex">
-      <a href="${projects[i].codeUrl}" target="_blank"><img src="img/icon/github.svg" alt="GitHub"></a>
-      <a href="${projects[i].liveUrl}" target="_blank"><img src="img/icon/arrow-up-right-from-square-solid.svg" alt="External link"></a>
-    </p>
-  </div>
-</div> `;
+	for (let i = 0; i < projects.length; i++) {
+		if(tag == 'all' || projects[i].tags.includes(tag)) {
+				output += `<div class="project">
+				<img src="img/project/${projects[i].img}" alt="Calculator Project Image">
+				<div class="content flex">
+					<p>${projects[i].name}</p>
+					<p class="flex">
+						<a href="${projects[i].codeUrl}" target="_blank"><img src="img/icon/github.svg" alt="GitHub"></a>
+						<a href="${projects[i].liveUrl}" target="_blank"><img src="img/icon/arrow-up-right-from-square-solid.svg" alt="External link"></a>
+					</p>
+				</div>
+			</div> `;
+		}
+	}
+	
+	projectsEl.innerHTML = output;
 }
 
-projectsEl.innerHTML = output;
+renderProjects('all');
